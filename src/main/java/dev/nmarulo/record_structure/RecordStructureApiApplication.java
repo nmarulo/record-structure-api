37abd5022100aef150ff81a3aec2ecd677fe5d35
Nicolás Marulanda P <nmarulo@outlook.com>
2025-05-31 00:43:54 +0200

    Init commit

---
+++ src/main/java/dev/nmarulo/record_structure/RecordStructureApiApplication.java
@@ -0,0 +1,13 @@
+package dev.nmarulo.record_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStructureApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RecordStructureApiApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/dev/nmarulo/record_structure/RecordStructureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nmarulo.record_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStructureApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
